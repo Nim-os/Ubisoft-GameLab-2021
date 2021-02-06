b52f03daaa24af96a62b5bb81a7110c576ed31d5
sophearahspsp <sophearahspsp@gmail.com>
2021-02-06 11:48:33 -0800

    player self propulsion
    hold down mouse button to add force & move in toward mouse direction, reset player location ("r") for testing

---
+++ Assets/Scripts/PlayerPropulsion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPropulsion : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    public float propulsionForce = 10f;
+    private Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        // hold down mouse button
+        if (Input.GetMouseButton(0))
+        {
+            // use mouse location to calculate direction to apply force
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float enter;
+            if (plane.Raycast(ray, out enter))
+            {
+                var hitPoint = ray.GetPoint(enter);
+                var mouseDirection = hitPoint - gameObject.transform.position;
+                mouseDirection = mouseDirection.normalized;
+                rigidbody.AddForce(mouseDirection * propulsionForce, ForceMode.Impulse);
+            }
+        }
+
+        // reset location for testing purposes
+        if (Input.GetKeyDown("r"))
+        {
+            this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            this.transform.position = Vector3.zero;
+        }
+    }
+}
