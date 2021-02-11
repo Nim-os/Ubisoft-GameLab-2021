0f8497a7c9b55b7d43f7ed5ed0e6bbe432bee35e
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-02-11 12:45:10 -0500

    Add room joining

--- Assets/Scripts/ServerManager.cs
+++ Assets/Scripts/ServerManager.cs
@@ -5,6 +5,8 @@ using Photon.Pun;
 
 public class ServerManager : MonoBehaviourPunCallbacks
 {
+	public const string DevRoomID = "dev";
+
 	void Start()
 	{
 		Debug.Log("Attempting to connect");
@@ -16,5 +18,33 @@ public class ServerManager : MonoBehaviourPunCallbacks
 	public override void OnConnectedToMaster()
 	{
 		Debug.Log("Connected to Photon master server");
+
+		Debug.Log("Attempting to join room");
+
+		PhotonNetwork.JoinRoom(DevRoomID);
+	}
+
+	public override void OnJoinedRoom()
+	{
+		Debug.Log("Successfully joined room");
+	}
+
+	public override void OnJoinRoomFailed(short returnCode, string message)
+	{
+		Debug.Log($"Failed to join room.\nError message: {returnCode}, {message}");
+
+		Debug.Log("Attempting to create room");
+
+		PhotonNetwork.CreateRoom(DevRoomID);
+	}
+
+	public override void OnCreatedRoom()
+	{
+		Debug.Log("Successfully created room");
+	}
+
+	public override void OnCreateRoomFailed(short returnCode, string message)
+	{
+		Debug.Log($"Failed to create room.\nError message: {message}");
 	}
 }
