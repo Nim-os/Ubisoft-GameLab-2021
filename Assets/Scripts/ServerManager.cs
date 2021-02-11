98a6bdec4d140cd16bf8e95d04bf9161132b4a3b
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-02-11 12:06:24 -0500

    Add ServerManager

---
+++ Assets/Scripts/ServerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ServerManager : MonoBehaviourPunCallbacks
+{
+	void Start()
+	{
+		Debug.Log("Attempting to connect");
+
+		PhotonNetwork.ConnectUsingSettings();
+	}
+
+
+	public override void OnConnectedToMaster()
+	{
+		Debug.Log("Connected to Photon master server");
+	}
+}
