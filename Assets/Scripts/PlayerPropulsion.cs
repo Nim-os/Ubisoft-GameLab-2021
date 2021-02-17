fc067c1e79b0bc28528c3d578202aa3d99056899
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-02-17 18:32:49 -0500

    Rig up propulsion to use new input system

--- Assets/Scripts/PlayerPropulsion.cs
+++ Assets/Scripts/PlayerPropulsion.cs
@@ -6,13 +6,29 @@ using Cinemachine;
 /// <summary> Contains all methods related to self propulsion </summary>
 public class PlayerPropulsion : MonoBehaviour
 {
+    public InputSystem input;
+
     public float propulsionForce;
     public int gas;
 
     private Rigidbody rb;
     private Plane plane = new Plane(Vector3.up, Vector3.zero);
 
-    void Start()
+    private bool isMe;
+
+    private Vector3 force = Vector3.zero;
+
+	void Awake()
+	{
+        input = new InputSystem();
+
+        input.Game.Primary.performed += x => OnPropulsion();
+        input.Game.Reset.performed += x => OnResetLocation();
+
+        isMe = gameObject.GetComponent<Photon.Pun.PhotonView>().IsMine;
+    }
+
+	void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
 
@@ -23,36 +39,41 @@ public class PlayerPropulsion : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (gameObject.GetComponent<Photon.Pun.PhotonView>().IsMine)
+        if (isMe && force != Vector3.zero)
 		{
-            OnPropulsion();
-            OnResetLocation();
-        }
+            rb.AddForce(force, ForceMode.Impulse);
+
+            force = Vector3.zero;
+		}
+        
     }
 
     /// <summary> On mouse button held down && not empty on gas, add force to player towards the mouse direction </summary>
-    private void OnPropulsion(){
-        if (Input.GetMouseButton(0) && gas > 0)
+    private void OnPropulsion()
+    {
+        if (isMe && gas > 0)
         {
             // Use up gas when propulsion
             gas--;
 
             // Use mouse location to calculate direction to apply force
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            float enter;
-            if (plane.Raycast(ray, out enter))
+
+            if (plane.Raycast(ray, out float enter))
             {
                 var hitPoint = ray.GetPoint(enter);
                 var mouseDirection = hitPoint - gameObject.transform.position;
                 mouseDirection = mouseDirection.normalized;
-                rb.AddForce(mouseDirection * propulsionForce, ForceMode.Impulse);
+
+                force = mouseDirection * propulsionForce;
             }
         }
     }
 
     /// <summary> Reset location of player on "r". Delete later. </summary>
-    private void OnResetLocation(){
-        if (Input.GetKeyDown("r"))
+    private void OnResetLocation()
+    {
+        if (isMe)
         {
             rb.velocity = Vector3.zero;
             this.transform.position = Vector3.zero;
