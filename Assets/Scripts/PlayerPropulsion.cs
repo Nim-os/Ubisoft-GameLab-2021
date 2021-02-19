05c0b840dc31f5f6f80a53f290afba6d6c51cdc9
sophearahspsp <sophearahspsp@gmail.com>
2021-02-18 22:26:50 -0800

    alternative player propulsion, hold down mouse button to build up power and let go to launch
    - separated GetMouseDirection() into its own method
    - Rock prefab for the player to launch when releasing the built up force
    - OnHold() builds up force when player is holding down on mouse button
    - OnLetGo() applies that built up force on the player in mouse direction. Also creates a rock that has force applied at the same magnitude and opposite direction of the player
    - created extra tester method to fill up player's gas

--- Assets/Scripts/PlayerPropulsion.cs
+++ Assets/Scripts/PlayerPropulsion.cs
@@ -8,7 +8,10 @@ public class PlayerPropulsion : MonoBehaviour
 {
     public float propulsionForce;
     public int gas;
+    public int holdingPower;
 
+    [SerializeField]
+    private GameObject rockPrefab;
     private Rigidbody rb;
     private Plane plane = new Plane(Vector3.up, Vector3.zero);
 
@@ -25,9 +28,58 @@ public class PlayerPropulsion : MonoBehaviour
     {
         if (gameObject.GetComponent<Photon.Pun.PhotonView>().IsMine)
 		{
-            OnPropulsion();
-            OnResetLocation();
+            // Hold down mouse button to build up force, let go to launch
+            OnHold();
+            OnLetGo();
+
+            // Test input methods
+            TestInputs();
         }
+    }
+
+    // rename this method better
+    /// <summary> When "holding", builds up force for the player to use when they let go </summary>
+    private void OnHold(){
+        if (Input.GetMouseButton(0) && gas > 0){
+            holdingPower++;
+            gas--;
+        }
+    }
+
+    // rename this method better
+    /// <summary> When "let go" applies force to player in mouse direction according to force built up & creates a rock and applies force on it in the opposite direction of the mouse</summary>
+    private void OnLetGo(){
+        Vector3 mouseDir;
+        if (!Input.GetMouseButton(0) && holdingPower != 0 && ((mouseDir = GetMouseDirection()) != Vector3.zero)){
+            // apply force on the player
+            rb.AddForce(mouseDir * propulsionForce * holdingPower, ForceMode.Impulse);
+
+            // create rock
+            GameObject rock = Instantiate(rockPrefab, transform.position + -mouseDir*2, transform.rotation);
+            float length = holdingPower*0.05f;
+            rock.transform.localScale = new Vector3(length, length, length);
+
+            // apply force 
+            rock.GetComponent<Rigidbody>().AddForce(-mouseDir * propulsionForce * holdingPower, ForceMode.Impulse);
+            
+            holdingPower = 0;
+        }
+    }
+
+    /// <summary> Gets mouse direction </summary>
+    private Vector3 GetMouseDirection(){
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float enter;
+
+        if (plane.Raycast(ray, out enter))
+        {
+            var hitPoint = ray.GetPoint(enter);
+            var mouseDirection = hitPoint - gameObject.transform.position;
+            mouseDirection = mouseDirection.normalized;
+            return mouseDirection;
+        }
+        // did not hit
+        return Vector3.zero;
     }
 
     /// <summary> On mouse button held down && not empty on gas, add force to player towards the mouse direction </summary>
@@ -57,5 +109,18 @@ public class PlayerPropulsion : MonoBehaviour
             rb.velocity = Vector3.zero;
             this.transform.position = Vector3.zero;
         }
+    }
+
+    /// <summary> Reset location of player on "g". Delete later. </summary>
+    private void OnFillUpGas(){
+        if (Input.GetKeyDown("g"))
+        {
+            gas = 100;
+        }
+    }
+
+    private void TestInputs(){
+        OnResetLocation();
+        OnFillUpGas();
     }
 }
