b8f1857939c9b5142eba3c2931578ed31b2a99a9
sophearahspsp <sophearahspsp@gmail.com>
2021-02-18 19:47:36 -0800

    camera height method

--- Assets/Scripts/PlayerPropulsion.cs
+++ Assets/Scripts/PlayerPropulsion.cs
@@ -11,10 +11,14 @@ public class PlayerPropulsion : MonoBehaviour
 
     private Rigidbody rb;
     private Plane plane = new Plane(Vector3.up, Vector3.zero);
+    private CinemachineTransposer cameraTransposer;
+    private float cameraHeight;
 
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        cameraTransposer = GameObject.Find("CM vcam1").GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTransposer>();
+        cameraHeight = cameraTransposer.m_FollowOffset.y;
 
         // Add player to cameraTargetGroup
         var cameraTargetGroup = GameObject.Find("CameraTargetGroup").GetComponent<CinemachineTargetGroup>();
@@ -34,6 +38,10 @@ public class PlayerPropulsion : MonoBehaviour
     private void OnPropulsion(){
         if (Input.GetMouseButton(0) && gas > 0)
         {
+            // move camera height
+            cameraHeight = cameraHeight - (float) 0.05;
+            SetCameraHeight(cameraHeight);
+
             // Use up gas when propulsion
             gas--;
 
@@ -48,6 +56,10 @@ public class PlayerPropulsion : MonoBehaviour
                 rb.AddForce(mouseDirection * propulsionForce, ForceMode.Impulse);
             }
         }
+    }
+
+    private void SetCameraHeight(float height){
+        cameraTransposer.m_FollowOffset.y = height;
     }
 
     /// <summary> Reset location of player on "r". Delete later. </summary>
