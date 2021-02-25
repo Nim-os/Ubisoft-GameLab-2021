f2243809eba26c2d5c4949bb4895086200e69f0c
sophearahspsp <sophearahspsp@gmail.com>
2021-02-24 18:41:39 -0800

    fixed another bug

--- Assets/Scripts/PlayerGravitation.cs
+++ Assets/Scripts/PlayerGravitation.cs
@@ -39,12 +39,13 @@ public class PlayerGravitation : MonoBehaviour
                 o.playerSelected = true;
                 currentSelection = o;
             }
-        }
         // else not holding RMB OR no objects to select
-        if (currentSelection != null){
-            currentSelection.playerSelected = false;
+        }else {
+            if (currentSelection != null){
+                currentSelection.playerSelected = false;
+            }
+            currentSelection = null;
         }
-        currentSelection = null;
     }
 
     private BaseGravitation SelectGravitationalObject(){
