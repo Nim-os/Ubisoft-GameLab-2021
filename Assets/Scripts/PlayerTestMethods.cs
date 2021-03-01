f5d93651a82426c7312675c31c8ceb2c8f4b9261
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-03-01 13:43:23 -0500

    Update to input system

--- Assets/Scripts/PlayerTestMethods.cs
+++ Assets/Scripts/PlayerTestMethods.cs
@@ -14,6 +14,7 @@ public class PlayerTestMethods : MonoBehaviour
 
     void Start()
     {
+        // If we don't own this script, we don't need it
         if (!gameObject.GetComponent<Photon.Pun.PhotonView>().IsMine)
 		{
             Destroy(this);
@@ -25,17 +26,18 @@ public class PlayerTestMethods : MonoBehaviour
         initMass = rb.mass;
         initScale = this.transform.localScale;
 
+
+        // Add input system and subscribe all functions
+
         input = new InputSystem();
 
-        
+        input.GameDebug.GasUp.performed += x => OnFillUpGas();
+        input.GameDebug.LogPos.performed += x => OnGetMousePosition();
+        input.GameDebug.ResetPos.performed += x => OnResetLocation();
     }
 
     void FixedUpdate()
     {
-        OnResetLocation();
-        OnFillUpGas();
-        OnGetMousePosition();
-
         if (rb.velocity.magnitude > 10) // What is this condition's purpose? Is it relevant to testing?
         {
             rb.velocity = Vector3.Normalize(rb.velocity) * 10;
@@ -43,28 +45,23 @@ public class PlayerTestMethods : MonoBehaviour
     }
 
     /// <summary> Reset location of player on "r".</summary>
-    private void OnResetLocation(){
-        if (Input.GetKeyDown("r"))
-        {
-            rb.velocity = Vector3.zero;
-            this.transform.position = Vector3.zero;
-        }
+    private void OnResetLocation()
+    {
+        rb.velocity = Vector3.zero;
+        this.transform.position = Vector3.zero;
     }
 
     /// <summary> Reset location of player on "g".</summary>
-    private void OnFillUpGas(){
-        if (Input.GetKeyDown("g"))
-        {
-            gameObject.GetComponent<PlayerPropulsion>().gas = initGas;
-            rb.mass = initMass;
-            transform.localScale = initScale;
-        }
+    private void OnFillUpGas()
+    {
+        gameObject.GetComponent<PlayerPropulsion>().gas = initGas;
+        rb.mass = initMass;
+        transform.localScale = initScale;
     }
 
     /// <summary> Print out Vector3 mouse position</summary>
-    private void OnGetMousePosition(){
-        if (Input.GetKeyDown("y")){
-            Debug.Log(Utils.mousePositionOnPlane());
-        }
+    private void OnGetMousePosition()
+    {
+        Debug.Log(Utils.mousePositionOnPlane());
     }
 }
