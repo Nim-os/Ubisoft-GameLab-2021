b9e98e8cae5cc5a3d96bcb6bb791a12324d1e180
Ness <vanessa.chu@mail.mcgill.ca>
2021-03-03 16:10:37 -0500

    Update Input to new InputSystem.

--- Assets/Scripts/DragReleaseArrow.cs
+++ Assets/Scripts/DragReleaseArrow.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class DragReleaseArrow : MonoBehaviour
 {
+    public InputSystem input;
+
     public GameObject arrowPrefab;
     GameObject currentArrow;
 
-    void Update()
+    Vector2 mousePos = Vector2.zero;
+    bool holding;
+
+    void Awake()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+        input = new InputSystem();
+
+        input.Game.Primary.performed += x => holding = true;
+        input.Game.Primary.canceled += x => holding = false;
+
+        input.Game.MousePosition.performed += x => mousePos = x.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        if (holding)
         {
             if (currentArrow == null)
             {
@@ -21,19 +35,29 @@ public class DragReleaseArrow : MonoBehaviour
             }
             if (gameObject.GetComponent<Photon.Pun.PhotonView>().IsMine)
             {
-                currentArrow.transform.localScale = new Vector3(Utils.DistanceMouseObj(gameObject) * .1f, 1, 1);
+                currentArrow.transform.localScale = new Vector3(Utils.DistanceMouseObj(mousePos, gameObject) * .1f, 1, 1);
 
-                Vector3 mousePos = Input.mousePosition;
+                Vector2 mousePosTemp = Vector2.zero;
                 Vector3 playerPos = Camera.main.WorldToScreenPoint(transform.position);
-                mousePos.x = mousePos.x - playerPos.x;
-                mousePos.y = mousePos.y - playerPos.y;
-                float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+                mousePosTemp.x = mousePos.x - playerPos.x;
+                mousePosTemp.y = mousePos.y - playerPos.y;
+                float angle = Mathf.Atan2(mousePosTemp.y, mousePosTemp.x) * Mathf.Rad2Deg;
                 currentArrow.transform.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));
             }
         }
-        if (Input.GetKeyUp(KeyCode.Mouse0))
+        if (!holding && currentArrow != null)
         {
             if (currentArrow != null) Destroy(currentArrow);
         }
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
     }
 }
