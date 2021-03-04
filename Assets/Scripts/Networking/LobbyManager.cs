405f91dd090ff9b33319de8d0d37404b0d1f3ade
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-03-04 12:35:05 -0500

    Add Button base logic

--- Assets/Scripts/Networking/LobbyManager.cs
+++ Assets/Scripts/Networking/LobbyManager.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LobbyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public TextMeshProUGUI logPanel;
+	public TextMeshProUGUI roomCode;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+	#region Buttons
+
+	public void CreateRoom()
+	{
+		Log($"Creating Room");
+
+
+
+
+		Log($"Created Room {1}");
+	}
+
+	public void JoinRoom()
+	{
+		Log($"Joining Room {roomCode.text}");
+
+
+
+
+		Log($"Joined Room {roomCode.text}");
+	}
+
+	public void PlayGame()
+	{
+		Log("Starting game.");
+
+
+	}
+
+	#endregion
+
+	private void Log(string text)
+	{
+		// TODO Make it more console like, messages don't overwrite but instead add themselves
+		//logPanel.text = text;
+		logPanel.SetText(text);
+	}
 }
