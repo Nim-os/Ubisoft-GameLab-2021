7ae641944d9e4ce61ab01131e2690cbfcbf06e4e
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-03-04 18:45:15 -0500

    Document code

--- Assets/Scripts/Networking/LobbyManager.cs
+++ Assets/Scripts/Networking/LobbyManager.cs
@@ -40,12 +40,16 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
 	#region Room Logic
 
+	/// <summary>
+	/// Creates a random room.
+	/// </summary>
 	public void CreateRoom()
 	{
 		Log($"Creating room...");
 
 		SetConnectionState(ConnectionState.Connecting);
 
+		// Room options to designate our needs
 		var room = new RoomOptions()
 		{
 			IsOpen = true,
@@ -55,15 +59,20 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
 		roomCode = GetRandomCode().ToString();
 
+		// Tell the network to create the room given the code and options
 		PhotonNetwork.CreateRoom(roomCode, room);
 	}
 
+	/// <summary>
+	/// Attempts to create the dev room and join it if it already exists.
+	/// </summary>
 	public void JoinDevRoom()
 	{
-		Log("Creating dev room...");
+		Log("Joining dev room...");
 
 		SetConnectionState(ConnectionState.Connecting);
 
+		// Dev room options to handle increased players
 		var room = new RoomOptions()
 		{
 			IsOpen = true,
@@ -73,11 +82,16 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
 		roomCode = DevRoomID;
 
+		// Attempt to create the dev room
 		PhotonNetwork.CreateRoom(DevRoomID, room);
 	}
 
+	/// <summary>
+	/// Handles the player attempting to join a room.
+	/// </summary>
 	public void JoinRoom()
 	{
+		// Check if the input field is whitespace first so that we don't freak out Photon
 		if (string.IsNullOrWhiteSpace(inputField.text))
 		{
 			Log("Must provide a room to join.");
@@ -88,19 +102,27 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
 			SetConnectionState(ConnectionState.Connecting);
 
+			// Attempt to join the room if the input is valid
 			PhotonNetwork.JoinRoom(inputField.text);
 		}
 	}
 
+	/// <summary>
+	/// Handles starting the game if player criterion is met.
+	/// </summary>
 	public void PlayGame()
 	{
 		Log("Starting game...");
 
 		SetConnectionState(ConnectionState.Started);
 
+		// Loads the initial level.
 		PhotonNetwork.LoadLevel(1); // TODO Change this to whatever scene, the number is the scene index in the build settings
 	}
 
+	/// <summary>
+	/// Handles the player leaving the room they are currently in.
+	/// </summary>
 	public void LeaveRoom()
 	{
 		Log("Left room.");
@@ -117,13 +139,14 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
 	public override void OnConnectedToMaster()
 	{
-		Log("Connected to the master server!");
+		Log("Connected to game server.");
 
 		SetConnectionState(ConnectionState.Idle);
 	}
 
 	public override void OnCreatedRoom()
 	{
+		// Set the input field to the room's code for clarity
 		inputField.SetTextWithoutNotify(roomCode);
 
 		Log($"Created room {roomCode} !");
@@ -131,16 +154,19 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
 	public override void OnCreateRoomFailed(short returnCode, string message)
 	{
+		// Checks if we were trying to create a dev room
 		if (roomCode.Equals(DevRoomID))
 		{
 			PhotonNetwork.JoinRoom(roomCode);
 		}
 		else
 		{
+			// Checks if the error code is related to using the code of an already existing room
 			if (returnCode == 32766)
 			{
 				Debug.Log("Recreating room, created duplicate.");
 
+				// Create a new room
 				CreateRoom();
 			}
 			else
@@ -174,6 +200,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
 	public override void OnJoinRoomFailed(short returnCode, string message)
 	{
+		// Tries to match the error code to see if we can handle it
 		switch (returnCode)
 		{
 			case 32758:
@@ -187,6 +214,8 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 				break;
 
 			default:
+				// Error code we aren't handling
+
 				Debug.LogError($"Failed to join room.\n{returnCode}: {message}");
 
 				Log("Unable to join room.");
@@ -198,6 +227,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
 	public override void OnLeftRoom()
 	{
+		// Clears the input field whenever we leave a room
 		inputField.SetTextWithoutNotify("");
 	}
 
@@ -218,6 +248,10 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 		logPanel.SetText(text);
 	}
 
+	/// <summary>
+	/// Generates a random room code.
+	/// </summary>
+	/// <returns>A short on the interval [10000, 65535)</returns>
 	private ushort GetRandomCode()
 	{
 		ushort code = (ushort) Random.Range(0x2710, 0xFFFF);
@@ -225,8 +259,15 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 		return code;
 	}
 
+	/// <summary>
+	/// Handles the interactability of UI input objects.
+	/// </summary>
+	/// <param name="state"></param>
 	private void SetConnectionState(ConnectionState state)
 	{
+		// Handles all the fun button interactivity so that we don't kill Photon by trying to create a room whilst being in one already.
+		// Nothing interesting here
+
 		switch (state)
 		{
 			case ConnectionState.Idle:
