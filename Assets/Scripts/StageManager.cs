1cff10db465552c58a0972c60f6452ad0c80b433
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-03-04 22:09:09 -0500

    Add spawn positions

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -4,7 +4,27 @@ using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
-    public List<Transform> spawns;
+    public GameObject spawnsParent;
 
-    
+	private List<Transform> spawns;
+
+	private void Awake()
+	{
+		spawns = new List<Transform>(spawnsParent.transform.childCount);
+
+		for (int i = 0; i < spawnsParent.transform.childCount; i++)
+		{
+			spawns.Add(spawnsParent.transform.GetChild(i));
+		}
+	}
+
+	private void Start()
+	{
+		SpawnPlayers();
+	}
+
+	private void SpawnPlayers()
+	{
+
+	}
 }
