64f67b55d5964601f83dae6b1e5e02cc9560b72c
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2021-03-04 22:24:29 -0500

    Add player spawning

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class StageManager : MonoBehaviour
 {
@@ -8,8 +10,14 @@ public class StageManager : MonoBehaviour
 
 	private List<Transform> spawns;
 
+	public Mesh p2_1;
+	public Material p2_1_mat;
+
 	private void Awake()
 	{
+		// Might be able to put this inside same if block as start since we don't need to do spawn positions
+		// Better to just let one player handle them
+
 		spawns = new List<Transform>(spawnsParent.transform.childCount);
 
 		for (int i = 0; i < spawnsParent.transform.childCount; i++)
@@ -20,11 +28,52 @@ public class StageManager : MonoBehaviour
 
 	private void Start()
 	{
-		SpawnPlayers();
+		if (ServerManager.instance.isHost)
+		{
+			if (ServerManager.instance.players.Count == 0)
+			{
+				Debug.LogError("No players in server manager. Did you try to run this scene directly? Use a lobby scene instead.");
+			}
+			else
+			{
+				SpawnPlayers();
+			}
+		}
 	}
 
 	private void SpawnPlayers()
 	{
+		int index = 0;
 
+		foreach (Player player in ServerManager.instance.players)
+		{
+			GameObject obj = GeneratePlayerGameObject(index);
+
+			var view = obj.GetComponent<PhotonView>();
+
+			view.TransferOwnership(player);
+
+			index += 1;
+		}
+	}
+
+	private GameObject GeneratePlayerGameObject(int index)
+	{
+		GameObject player = PhotonNetwork.Instantiate("Player", PickSpawnPosition(), Quaternion.identity);
+
+		if (index % 2 == 0)
+		{
+			Mesh p2_1_inst = Instantiate(p2_1);
+			player.GetComponent<MeshFilter>().mesh = p2_1_inst;
+			player.GetComponent<MeshRenderer>().material = p2_1_mat;
+		}
+
+		return player;
+	}
+
+	private Vector3 PickSpawnPosition()
+	{
+
+		return spawns[0].position;
 	}
 }
