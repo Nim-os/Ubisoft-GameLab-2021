7966725b77e2c8676eff18b3b042fd2edb35321d
sophearahspsp <sophearahspsp@gmail.com>
2021-03-07 10:20:23 -0800

    PlayerAbsorption.cs style cleanup

--- Assets/GameLab/Scripts/Player/PlayerAbsorption.cs
+++ Assets/GameLab/Scripts/Player/PlayerAbsorption.cs
@@ -6,41 +6,36 @@ public class PlayerAbsorption : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody rb;
-    int counter; //mimic the logarithmic growth
-    public float startLogAt=0; //to start logarithmic growth once the size reach this number
+    int counter;
+    public float startLogAt = 0;
+
     void Start()
     {
-        rb=gameObject.GetComponent<Rigidbody>();
-        counter=0;
+        rb = gameObject.GetComponent<Rigidbody>();
+        counter = 0;
     }
 
     void OnCollisionEnter(Collision collision){
         //check if the collision is between 2 players
-        if(collision.rigidbody.gameObject.tag=="Player"){
-            //then it would be the end of the game
-            //TODO: game over function should be here
+        if(collision.rigidbody.gameObject.tag == "Player"){
             print("Game Over!!!");
         }
         
-
         //destroy the rigidbody
-        float mass=0;
+        float mass = 0;
         if(collision.rigidbody){
-            mass=collision.rigidbody.mass;
+            mass = collision.rigidbody.mass;
             Destroy(collision.rigidbody.gameObject);
         }
 
         //add the mass to the rigidbody
         //mimic logarithmic growth
-        if(rb.mass>=startLogAt){
+        if(rb.mass >= startLogAt){
             //start log once the mass pass the threshold
             counter++;
-            rb.mass+= mass*(1/counter);
+            rb.mass += mass * (1/counter);
         }else{
-            rb.mass+= mass;
+            rb.mass += mass;
         }
-        
-        
-        
     }
 }
