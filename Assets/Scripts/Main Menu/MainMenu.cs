dfe1cb71deed66256846ed697c2857e1f7b65c7c
AprilDeFeu <aprildefeu@protonmail.com>
2021-03-07 00:32:43 -0500

    April's Main Menu with parallax and music
    I implemented a working main menu with volume control, credits, and an easy to change script for the scene manager. As such, one needs to go into the MainMenu script and set the proper scene needed to start the game.
    
    Moreover, the parallax script works for the background perfectly as intended, and I wrote it such that it works on both the x and z axes. All that needs to be done is to change the parallax effect parameters for each layer of sprites/objects that need to be under the parallax effect.
    
    Lastly, I cleaned up the main theme so we have a nice spacey jam. Also, I used 'Worldlines' as a working title, which is just paths in spacetime. Feel free to change it, it's there as a placeholder.

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public AudioMixer mix;
+    public void PlayGame()
+    {
+        // Currently set to next scene in Build queue, but can be changed to the lobby or tutorial level
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        // Currently set to next scene in Build queue, but can be changed to the lobby or tutorial level
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void SetVolume (float vol)
+    {
+        mix.SetFloat("volume", vol);
+    }
+
+
+}
