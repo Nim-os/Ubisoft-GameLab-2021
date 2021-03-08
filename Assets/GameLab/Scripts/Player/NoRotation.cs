faa0666939c76f8cbf16d8a4ad138dd22d46f7e4
sophearahspsp <sophearahspsp@gmail.com>
2021-03-07 19:46:22 -0800

    organizing file structure (player scripts in their own folder)

---
+++ Assets/GameLab/Scripts/Player/NoRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoRotation : MonoBehaviour
+{
+    private Transform t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t.eulerAngles = new Vector3 (0, 0, 0);
+    }
+}
