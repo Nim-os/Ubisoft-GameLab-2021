558269b855af8903c64d3270638cc62f8ecde1ac
sophearahspsp <sophearahspsp@gmail.com>
2021-03-08 00:28:58 -0800

    GenerateTexture.cs, remove texture gen in BaseAsteroid.cs, modified prefabs to generate textures

---
+++ Assets/GameLab/Scripts/Stage/GenerateTexture.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTexture : MonoBehaviour
+{
+    [SerializeField]
+    private List<Mesh> models;
+    [SerializeField]
+    private List<Material> textures;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int choice = Random.Range(0,models.Count);
+        SetModelTexture(models[choice], textures[choice]);
+    }
+
+    private void SetModelTexture(Mesh obj, Material mat){
+        GetComponent<MeshFilter>().mesh = obj;
+        GetComponent<MeshRenderer>().material = mat;
+    }
+}
