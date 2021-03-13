19a7e8df095a328575e961426c814a38ab8a708b
moyang.chen@mail.mcgill.ca <moyang.chen@mail.mcgill.ca>
2021-03-13 13:51:22 -0500

    make scrollbar auto scroll to the bottom when there's new message

---
+++ Assets/GameLab/Scripts/UI/ScrollIssue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollIssue : MonoBehaviour
+{
+    GameObject content;
+    double height;
+
+    void Start(){
+        //record the height of the current rect
+        content=this.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject;
+        height=content.GetComponent<RectTransform>().rect.height;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update the position of the scroll rect once the height change
+        if(content.GetComponent<RectTransform>().rect.height>height){
+            this.GetComponent<ScrollRect>().normalizedPosition = new Vector2(0, 0);
+            height=content.GetComponent<RectTransform>().rect.height;
+        }
+        
+    }
+}
