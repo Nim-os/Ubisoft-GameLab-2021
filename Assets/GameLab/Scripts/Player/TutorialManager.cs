bb1a48dfd8d58e919090b13ef02d9c898eeae5b7
Ness <vanessa.chu@mail.mcgill.ca>
2021-03-13 15:16:39 -0500

    Resolve NullRef error in tutorial script.

--- Assets/GameLab/Scripts/Player/TutorialManager.cs
+++ Assets/GameLab/Scripts/Player/TutorialManager.cs
@@ -47,120 +47,122 @@ public class TutorialManager : MonoBehaviour
             }
         }
 
-        // Sun chases players.
-        if (sunChasing)
+        else
         {
-            if (Vector3.Distance(player.transform.position, sun.transform.position) > 50) sun.transform.position = new Vector3(sun.transform.position.x + (9 * Time.deltaTime), 0, player.transform.position.z);
-            else sun.transform.position = new Vector3(sun.transform.position.x + (1 * Time.deltaTime), 0, player.transform.position.z);
-        }
-
-        if (state == 0) // Propulsion
-        {
-            player.GetComponent<PlayerAbsorption>().enabled = false;
-            // TODO: Absorption script still runs --> Need to add isEnabled check to PlayerAbsorption.cs
-
-            // Set up stage
-            if (markers.Count == state)
+            // Sun chases players.
+            if (sunChasing)
             {
-                SetUp("Hold/Tap LMB to propulse.");
+                if (Vector3.Distance(player.transform.position, sun.transform.position) > 50) sun.transform.position = new Vector3(sun.transform.position.x + (9 * Time.deltaTime), 0, player.transform.position.z);
+                else sun.transform.position = new Vector3(sun.transform.position.x + (1 * Time.deltaTime), 0, player.transform.position.z);
             }
-            // Condition to proceed
-            else if ((Camera.main.WorldToScreenPoint(firstPlanet.transform.position).x < Screen.width - 40) && markers[state].enabled)
-            {
-                TearDown();
-            }
-        }
 
-        if (state == 1) // Gravitation
-        {
-            // Set up stage
-            if (markers.Count == state)
+            if (state == 0) // Propulsion
             {
-                SetUp("Hold RMB to gravitate.");
-            }
-            // Condition to proceed
-            else if (holdingRMB && player.GetComponent<PlayerGravitation>().currentSelection != null && markers[state].enabled)
-            {
-                TearDown();
-            }
-        }
+                player.GetComponent<PlayerAbsorption>().enabled = false;
+                // TODO: Absorption script still runs --> Need to add isEnabled check to PlayerAbsorption.cs
 
-        if (state == 2) // Gravitation between players
-        {
-            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-            player.GetComponent<PlayerAbsorption>().enabled = true;
-
-            // Set up stage
-            if (markers.Count == state)
-            {
-                SetUp("Hold RMB to gravitate to your friend. You can take turns propulsing and gravitating to save resources. Careful: Don't crash!");
-                if (players.Length < 2)
+                // Set up stage
+                if (markers.Count == state)
                 {
-                    testPlayer = Instantiate(basicPlanet, new Vector3(100, 1, player.transform.position.z - 10), Quaternion.identity);
-                    testPlayer.tag = "Player";
-                    players = new GameObject[] { player, testPlayer };
+                    SetUp("Hold/Tap LMB to propulse.");
+                }
+                // Condition to proceed
+                else if ((Camera.main.WorldToScreenPoint(firstPlanet.transform.position).x < Screen.width - 40) && markers[state].enabled)
+                {
+                    TearDown();
                 }
             }
-            // Condition to proceed
-            else if (Vector3.Distance(players[0].transform.position, players[1].transform.position) < 15 && markers[state].enabled)
-            {
-                TearDown();
-                Destroy(testPlayer);
-                previousMass = player.GetComponent<Rigidbody>().mass;
-            }
-            else print(Vector3.Distance(players[0].transform.position, players[1].transform.position));
-        }
 
-        if (state == 3) // Mass Ejection
-        {
-            // Set up stage
-            if (markers.Count == state)
+            if (state == 1) // Gravitation
             {
-                SetUp("Hold and release z to eject mass.");
+                // Set up stage
+                if (markers.Count == state)
+                {
+                    SetUp("Hold RMB to gravitate.");
+                }
+                // Condition to proceed
+                else if (holdingRMB && player.GetComponent<PlayerGravitation>().currentSelection != null && markers[state].enabled)
+                {
+                    TearDown();
+                }
             }
-            // Condition to proceed // TODO: Check that mass was lost due to ejection rather than propulsion. Input system?
-            else if (player.GetComponent<Rigidbody>().mass < previousMass && markers[state].enabled)
-            {
-                TearDown();
-                previousMass = player.GetComponent<Rigidbody>().mass;
-            }
-            else previousMass = player.GetComponent<Rigidbody>().mass;
 
-        }
+            if (state == 2) // Gravitation between players
+            {
+                GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+                player.GetComponent<PlayerAbsorption>().enabled = true;
 
-        if (state == 4) // Mass Absorption
-        {
-            // Set up stage
-            if (markers.Count == state)
-            {
-                SetUp("Collide with mass smaller than yourself to absorb it.");
-                // Spawn some asteroids.
-                Instantiate(basicAsteroid, new Vector3(130, 1, player.transform.position.z + 5), Quaternion.identity);
-                Instantiate(basicAsteroid, new Vector3(140, 1, player.transform.position.z - 5), Quaternion.identity);
-                Instantiate(basicAsteroid, new Vector3(150, 1, player.transform.position.z + 5), Quaternion.identity);
-                Instantiate(basicAsteroid, new Vector3(160, 1, player.transform.position.z - 5), Quaternion.identity);
-                Instantiate(basicAsteroid, new Vector3(170, 1, player.transform.position.z + 5), Quaternion.identity);
+                // Set up stage
+                if (markers.Count == state)
+                {
+                    SetUp("Hold RMB to gravitate to your friend. You can take turns propulsing and gravitating to save resources. Careful: Don't crash!");
+                    if (players.Length < 2)
+                    {
+                        testPlayer = Instantiate(basicPlanet, new Vector3(100, 1, player.transform.position.z - 10), Quaternion.identity);
+                        testPlayer.tag = "Player";
+                        players = new GameObject[] { player, testPlayer };
+                    }
+                }
+                // Condition to proceed
+                else if (Vector3.Distance(players[0].transform.position, players[1].transform.position) < 15 && markers[state].enabled)
+                {
+                    TearDown();
+                    Destroy(testPlayer);
+                    previousMass = player.GetComponent<Rigidbody>().mass;
+                }
             }
-            // Condition to proceed
-            else if (player.GetComponent<Rigidbody>().mass > previousMass && markers[state].enabled)
-            {
-                TearDown();
-            }
-            else previousMass = player.GetComponent<Rigidbody>().mass;
-        }
 
-        if (state == 5) // Navigate to the end
-        {
-            // Set up stage
-            if (markers.Count == state)
+            if (state == 3) // Mass Ejection
             {
-                SetUp("Escape the solar system!");
+                // Set up stage
+                if (markers.Count == state)
+                {
+                    SetUp("Hold and release z to eject mass.");
+                }
+                // Condition to proceed // TODO: Check that mass was lost due to ejection rather than propulsion. Input system?
+                else if (player.GetComponent<Rigidbody>().mass < previousMass && markers[state].enabled)
+                {
+                    TearDown();
+                    previousMass = player.GetComponent<Rigidbody>().mass;
+                }
+                else previousMass = player.GetComponent<Rigidbody>().mass;
+
             }
-            // Condition to proceed
-            else if(player.transform.position.x >= 9999)
+
+            if (state == 4) // Mass Absorption
             {
-                sunChasing = false;
-                // Go to next scene.
+                // Set up stage
+                if (markers.Count == state)
+                {
+                    SetUp("Collide with mass smaller than yourself to absorb it.");
+                    // Spawn some asteroids.
+                    Instantiate(basicAsteroid, new Vector3(130, 1, player.transform.position.z + 5), Quaternion.identity);
+                    Instantiate(basicAsteroid, new Vector3(140, 1, player.transform.position.z - 5), Quaternion.identity);
+                    Instantiate(basicAsteroid, new Vector3(150, 1, player.transform.position.z + 5), Quaternion.identity);
+                    Instantiate(basicAsteroid, new Vector3(160, 1, player.transform.position.z - 5), Quaternion.identity);
+                    Instantiate(basicAsteroid, new Vector3(170, 1, player.transform.position.z + 5), Quaternion.identity);
+                }
+                // Condition to proceed
+                else if (player.GetComponent<Rigidbody>().mass > previousMass && markers[state].enabled)
+                {
+                    TearDown();
+                }
+                else previousMass = player.GetComponent<Rigidbody>().mass;
+            }
+
+            if (state == 5) // Navigate to the end
+            {
+                // Set up stage
+                if (markers.Count == state)
+                {
+                    SetUp("Escape the solar system!");
+                }
+                // Condition to proceed
+                else if (player.transform.position.x >= 9999)
+                {
+                    sunChasing = false;
+                    // Go to next scene.
+                }
             }
         }
     }
