f2b892bcd6ac0bf7d32506530ca61b220750a9e8
AprilDeFeu <aprildefeu@protonmail.com>
2021-03-14 17:25:44 -0400

    Multiple changes
    First, I added comments to both parallax scripts. The Main Menu one works on a different plane than the Objects one, so they must stay separate. I also renamed each one according to Soph's comments on my previous PR.
    
    Next, I added music to the main gameplay, parallax, and an audio button. Note that I need to add a script that prevents movement when the player clicks here, but it works to lower the volume as intended.
    
    Finally, I adjusted the lobby UI to include parallax, music, and an appropiate font with a nice colorblind friendly palette (the same one from the MM).

--- Assets/GameLab/Scripts/UI/ParallaxMainMenu.cs
+++ Assets/GameLab/Scripts/UI/ParallaxMainMenu.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParallaxBackground : MonoBehaviour
+public class ParallaxMainMenu : MonoBehaviour
 {
-    public float _parallaxEffectX = .5f;
-    public float _parallaxEffectZ = .5f;
+    // Note: This parallax script works on a different plane than the 
+    // script "ParallaxBackgroundObject", and thus is required to keep
+    // separate
+    
+    // Value of parallax effect
+    public float _parallaxEffect = .5f;
+    
+    // Texture unit size 
     float textureUnitSizeX;
     float textureUnitSizeZ;
 
@@ -13,8 +19,11 @@ public class ParallaxBackground : MonoBehaviour
     Vector3 previous;
     private void Start()
     {
+        // Get previous camera position and current camera position
         cam = Camera.main.transform;
         previous = cam.position;
+
+        // Initialize sprite unit sizes
         Sprite sprite = GetComponent<SpriteRenderer>().sprite;
         Texture2D texture = sprite.texture;
         textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
@@ -23,16 +32,18 @@ public class ParallaxBackground : MonoBehaviour
 
     private void LateUpdate()
     {
+        // Difference of current and previous cam position to give parallax effect.
         Vector3 delta = cam.position - previous;
-        transform.position += Vector3.right * delta.x * _parallaxEffectX + Vector3.forward * delta.z * _parallaxEffectZ;
+        transform.position += Vector3.right * delta.x * _parallaxEffect + Vector3.forward * delta.z * _parallaxEffect;
         previous = cam.position;
 
+        // Limits to repeat background on x axis
         if (cam.position.x - transform.position.x >= textureUnitSizeX)
         {
             float offset = (cam.position.x - transform.position.x) % textureUnitSizeX;
             transform.position = Vector3.right * (cam.position.x + offset) + Vector3.forward * transform.position.z;
         }
-
+        // Limits to repeat background on z axis
         if (cam.position.z - transform.position.z >= textureUnitSizeZ)
         {
             float offset = (cam.position.z - transform.position.z) % textureUnitSizeZ;
