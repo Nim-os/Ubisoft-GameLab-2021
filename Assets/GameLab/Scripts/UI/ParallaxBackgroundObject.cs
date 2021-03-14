8f4c396f8cb2c9dea3b29609ed1451ef38fb961f
AprilDeFeu <aprildefeu@protonmail.com>
2021-03-14 17:25:44 -0400

    Multiple changes
    First, I added comments to both parallax scripts. The Main Menu one works on a different plane than the Objects one, so they must stay separate. I also renamed each one according to Soph's comments on my previous PR.
    
    Next, I added music to the main gameplay, parallax, and an audio button. Note that I need to add a script that prevents movement when the player clicks here, but it works to lower the volume as intended.
    
    Finally, I adjusted the lobby UI to include parallax, music, and an appropiate font with a nice colorblind friendly palette (the same one from the MM).

--- Assets/GameLab/Scripts/UI/ParallaxBackgroundObject.cs
+++ Assets/GameLab/Scripts/UI/ParallaxBackgroundObject.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParallaxGame : MonoBehaviour
+public class ParallaxBackgroundObject : MonoBehaviour
 {
+    // This script works on a different plane than "Parallax Main Menu"
+    
+    // Sprite size & position
     private float spriteLength, spriteWidth;
     Vector3 startPosition;
+    // Camera
     public GameObject cam;
+    // Effect parameter
     public float _parallaxEffect;
 
 
@@ -14,20 +19,25 @@ public class ParallaxGame : MonoBehaviour
     void Start()
     {
         startPosition = transform.position;
-        spriteLength = GetComponent<SpriteRenderer>().bounds.size.x/3;
-        spriteWidth = GetComponent<SpriteRenderer>().bounds.size.z/3;
+        spriteLength = GetComponent<SpriteRenderer>().bounds.size.x / 3;
+        spriteWidth = GetComponent<SpriteRenderer>().bounds.size.z / 3;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Frame of reference for parallax effect
         Vector3 galilean = (cam.transform.position * (1 - _parallaxEffect));
+        // Frame of reference for observer
         Vector3 distance = (cam.transform.position * _parallaxEffect);
+        // Galilean transform to compute parallax
         transform.position = new Vector3(startPosition.x + distance.x, transform.position.y, startPosition.z + distance.z);
 
+        // Limits on galilean transform for parallax on x axis
         if (galilean.x > startPosition.x + spriteLength) startPosition.x += spriteLength;
         else if (galilean.x < startPosition.x - spriteLength) startPosition.x -= spriteLength;
 
+        // Limits on galilean transform for parallax on z axis
         if (galilean.z > startPosition.z + spriteWidth) startPosition.z += spriteWidth;
         else if (galilean.z < startPosition.z - spriteWidth) startPosition.z -= spriteWidth;
     }
