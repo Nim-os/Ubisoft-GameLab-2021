672dd053fd606c4dc8efcaebff98d9ca68f311ca
Ness <vanessa.chu@mail.mcgill.ca>
2021-03-15 00:25:09 -0400

    Complete Tutorial Stage gameplay and bugfixes.
    Can make final escape stage more complex.

--- Assets/GameLab/Scripts/Player/TutorialManager.cs
+++ Assets/GameLab/Scripts/Player/TutorialManager.cs
@@ -10,11 +10,15 @@ public class TutorialManager : MonoBehaviour
 
     int state = 0;
     List<Image> markers = new List<Image>();
-    
-    public GameObject basicPlanet;
-    public GameObject basicAsteroid;
+
+    [SerializeField]
+    private GameObject basicPlanet;
+    [SerializeField]
+    private GameObject basicAsteroid;
+
     public GameObject sun;
     GameObject firstPlanet;
+    public GameObject endCluster;
     public Canvas canv;
     public Image arrow;
     GameObject testPlayer; // For testing.
@@ -51,9 +55,9 @@ public class TutorialManager : MonoBehaviour
                 else sun.transform.position = new Vector3(sun.transform.position.x + (1 * Time.deltaTime), 0, player.transform.position.z);
             }
 
-            if (state == 0) Propulsion(); // Propulse with LMB
-            if (state == 1) GravitationToPlanet(); // Gravitate to basic planet with RMB
-            if (state == 2) GravitationToPlayer(); // Approach other player with RMB/propulse
+            if (state == 0) GravitationToPlayer(); // Approach other player with RMB/propulse
+            if (state == 1) Propulsion(); // Propulse with LMB
+            if (state == 2) GravitationToPlanet(); // Gravitate to basic planet with RMB
             if (state == 3) MassEjection(); // Eject mass with 'z'
             if (state == 4) MassAbsorption(); // Absorb small mass on collision
             if (state == 5) Escape(); // Escape an obstacle field with sun enabled
@@ -78,6 +82,28 @@ public class TutorialManager : MonoBehaviour
     }
 
     #region Tutorial Stages
+    void GravitationToPlayer()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+        if (markers.Count == state)
+        {
+            SetUp("Hold RMB to gravitate to your friend. You can take turns propulsing and gravitating to save resources. Careful: Don't crash!");
+            if (players.Length < 2) // To be removed when done testing the Tutorial.
+            {
+                testPlayer = Instantiate(basicPlanet, new Vector3(15, 1, player.transform.position.z - 10), Quaternion.identity);
+                testPlayer.tag = "Player";
+                players = new GameObject[] { player, testPlayer };
+            }
+        }
+        // Proceeds to the next stage if players are close enough wehether due to gravity or propulsion.
+        else if (Vector3.Distance(players[0].transform.position, players[1].transform.position) < 15 && markers[state].enabled)
+        {
+            TearDown();
+            Destroy(testPlayer);
+            previousMass = player.GetComponent<Rigidbody>().mass;
+        }
+    }
     void Propulsion()
     {
         if (markers.Count == state)
@@ -104,36 +130,13 @@ public class TutorialManager : MonoBehaviour
             TearDown();
         }
     }
-    void GravitationToPlayer()
-    {
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        
-        if (markers.Count == state)
-        {
-            SetUp("Hold RMB to gravitate to your friend. You can take turns propulsing and gravitating to save resources. Careful: Don't crash!");
-            if (players.Length < 2) // To be removed when done testing the Tutorial.
-            {
-                testPlayer = Instantiate(basicPlanet, new Vector3(100, 1, player.transform.position.z - 10), Quaternion.identity);
-                testPlayer.tag = "Player";
-                players = new GameObject[] { player, testPlayer };
-            }
-        }
-        // Proceeds to the next stage if players are close enough wehether due to gravity or propulsion.
-        else if (Vector3.Distance(players[0].transform.position, players[1].transform.position) < 15 && markers[state].enabled)
-        {
-            TearDown();
-            Destroy(testPlayer);
-            previousMass = player.GetComponent<Rigidbody>().mass;
-        }
-    }
     void MassEjection()
     {
         if (markers.Count == state)
         {
             SetUp("Hold and release z to eject mass.");
         }
-        // TODO: Check that mass was lost due to ejection rather than propulsion. Input system?
-        else if (player.GetComponent<Rigidbody>().mass < previousMass && markers[state].enabled)
+        else if (player.GetComponent<PlayerProjectile>().holding && markers[state].enabled)
         {
             TearDown();
         }
@@ -146,14 +149,13 @@ public class TutorialManager : MonoBehaviour
             SetUp("Collide with mass smaller than yourself to absorb it.");
             // Spawn some asteroids.
             // TODO: Allow instantiations from one player to be seen as ghostly/uninteractable obstacles on the other player's screen.
-            Instantiate(basicAsteroid, new Vector3(130, 1, player.transform.position.z + 5), Quaternion.identity);
-            Instantiate(basicAsteroid, new Vector3(140, 1, player.transform.position.z - 5), Quaternion.identity);
-            Instantiate(basicAsteroid, new Vector3(150, 1, player.transform.position.z + 5), Quaternion.identity);
-            Instantiate(basicAsteroid, new Vector3(160, 1, player.transform.position.z - 5), Quaternion.identity);
-            Instantiate(basicAsteroid, new Vector3(170, 1, player.transform.position.z + 5), Quaternion.identity);
+            for (int i = 0; i < 5; i++)
+            {
+                Instantiate(basicAsteroid, new Vector3(100 + (i * 10), 1, player.transform.position.z), Quaternion.identity);
+            }
         }
         // Proceeds to next stage after player is mostly through the asteroid field. 
-        else if (player.transform.position.x > 155 && markers[state].enabled)
+        else if (player.transform.position.x > 130 && markers[state].enabled)
         {
             TearDown();
         }
@@ -165,9 +167,11 @@ public class TutorialManager : MonoBehaviour
             // TODO: Instantiate obstacles for player to navigate.
             SetUp("Escape the solar system!");
             sunChasing = true;
-            sun.transform.position = new Vector3(130, 1, player.transform.position.z);
+            sun.transform.position = new Vector3(60, 1, player.transform.position.z);
+            endCluster.SetActive(true);
+            endCluster.transform.position = new Vector3(player.transform.position.x + 25, 1, player.transform.position.z);
         }
-        else if (player.transform.position.x >= 9999)
+        else if (player.transform.position.x >= endCluster.transform.position.x + 100)
         {
             sunChasing = false;
             // Go to next scene.
