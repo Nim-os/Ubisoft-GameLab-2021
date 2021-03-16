a2e95da4a80ab0afc7ea9447c37f074964ba315d
sophearahspsp <sophearahspsp@gmail.com>
2021-03-16 14:21:06 -0700

    progress so far on animated planets

---
+++ Assets/GameLab/Scripts/Stage/RandomizeRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeRotation : MonoBehaviour
+{
+    Animator animator;
+    public float upperRandom = 100f;
+    public float lowerRandom = 1f;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        //animator.speed = Random.Range(lowerRandom,upperRandom);
+        transform.localRotation = Random.rotation;
+    }
+}
