ee6e66a8ee4d34a7d13ad337167e16bcdbfd4f4b
sophearahspsp <sophearahspsp@gmail.com>
2021-03-16 17:20:43 -0700

    finished randomize animation speed and rotation start

--- Assets/GameLab/Scripts/Stage/RandomizeRotation.cs
+++ Assets/GameLab/Scripts/Stage/RandomizeRotation.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class RandomizeRotation : MonoBehaviour
 {
     Animator animator;
-    public float upperRandom = 100f;
-    public float lowerRandom = 1f;
+    public float upperRandom = 2f;
+    public float lowerRandom = 0.08f;
      
     // Start is called before the first frame update
     void Start()
     {
-        animator = gameObject.GetComponent<Animator>();
-        //animator.speed = Random.Range(lowerRandom,upperRandom);
+        animator = gameObject.transform.GetChild(0).GetComponent<Animator>();
+        animator.speed = Random.Range(lowerRandom,upperRandom);
         transform.localRotation = Random.rotation;
     }
 }
