7c64e110a356e48f0231e76073d9b993ed979223
sophearahspsp <sophearahspsp@gmail.com>
2021-03-18 00:27:12 -0700

    added base speed, added rotation script to prefabs, remove gravitational scripts from lobby planets

--- Assets/GameLab/Scripts/Stage/RandomRotationAnim.cs
+++ Assets/GameLab/Scripts/Stage/RandomRotationAnim.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RandomRotationAnim : MonoBehaviour
 {
-    public float speed;
+    public float speed = 1;
     [SerializeField] private Rigidbody rb;
 
     // Start is called before the first frame update
