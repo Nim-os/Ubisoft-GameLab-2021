715fb93f5b65a9ddc131f24e1c606cf15e1fec83
sophearahspsp <sophearahspsp@gmail.com>
2021-03-17 23:48:35 -0700

    random rotation animation script

---
+++ Assets/GameLab/Scripts/Stage/RandomRotationAnim.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotationAnim : MonoBehaviour
+{
+    public float speed;
+    [SerializeField] private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.angularVelocity = Random.insideUnitSphere * speed;
+    }
+}
