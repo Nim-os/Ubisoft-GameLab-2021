2cdcbea926b89832c52fbf87921ab12178dc56cd
AprilDeFeu <aprildefeu@protonmail.com>
2021-03-22 13:06:39 -0400

    Audio Sync and canvas fix
    Audio slider is synced in every scene along with the audio. Also fixed canvas size to stay the same regardless of resolution change.

---
+++ Assets/GameLab/Scripts/UI/AudioSync.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioSync : MonoBehaviour
+{
+    // get Audio slider
+    public Slider audioSlider;
+    public AudioMixer mix;
+    void Awake ()
+    {
+        // Use PlayerPrefs to get Key containing volume float.
+        if (audioSlider != null && PlayerPrefs.HasKey("Volume")) 
+        {
+            float currentVolume = PlayerPrefs.GetFloat("Volume");
+            audioSlider.value = currentVolume;
+            mix.SetFloat("Volume", currentVolume);
+        }
+    }
+
+    void Update ()
+    {
+        mix.SetFloat("Volume", audioSlider.value);
+
+        PlayerPrefs.SetFloat("Volume", audioSlider.value);
+        PlayerPrefs.Save();
+        
+    }
+}
