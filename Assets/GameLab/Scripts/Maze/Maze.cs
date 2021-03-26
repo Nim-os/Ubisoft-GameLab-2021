295d8657d340d4a32dedf5b31c21776737911dd8
AprilDeFeu <aprildefeu@protonmail.com>
2021-03-25 23:32:10 -0400

    Maze Photon-update
    Dunno if the instantiation works online, but it works well offline. Using the BasicPlanet asset from the Photon instantiate now and also checks if the player is Master Client to generate the planets.Also removed repetitive comments on 'Maze' script.

--- Assets/GameLab/Scripts/Maze/Maze.cs
+++ Assets/GameLab/Scripts/Maze/Maze.cs
@@ -5,8 +5,6 @@ public class Maze : MonoBehaviour
 {   [SerializeField]
     private int _mazeRows, _mazeColumns;
     [SerializeField]
-    private GameObject[] _planets;
-    [SerializeField]
     private float _spacing = 30f;
     [SerializeField]
     private int _planetSizeMinimum=2, _planetSizeMaximum=10;
@@ -15,9 +13,12 @@ public class Maze : MonoBehaviour
 
     void Start()
     {
-        InitializeMaze ();
-        Algorithm alg = new HuntKill (maze);
-        alg.CreateMaze();
+        if (PhotonNetwork.IsMasterClient)
+        {
+            InitializeMaze ();
+            Algorithm alg = new HuntKill (maze);
+            alg.CreateMaze();
+        }
     }
 
     void InitializeMaze ()
@@ -37,67 +38,52 @@ public class Maze : MonoBehaviour
                 // Instantiate West planet
                 if (col == 0)
                 {
-                    // Get a random prefab
-                    GameObject planetW = _planets[Random.Range(0,_planets.Length)];
                     // Set random size given a range
                     int sizeW = Random.Range(_planetSizeMinimum, _planetSizeMaximum+1);
                     // Instantiate using Photon
-                    maze[row,col].westPlanet = Instantiate(planetW, this.transform.position+new Vector3 (row * _spacing, 0, col*_spacing - _spacing/2f), Quaternion.identity);
+                    maze[row,col].westPlanet = PhotonNetwork.Instantiate("BasicPlanet", this.transform.position+new Vector3 (row * _spacing, 0, col*_spacing - _spacing/2f), Quaternion.identity);
                     // Set random size
                     maze[row,col].westPlanet.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f) * sizeW;
                     // Set inside GameObject where maze is initiated for cleaner scene hierarchy
                     maze[row,col].westPlanet.transform.parent = this.transform;
                     // Change name of object with relative coordinate attached;
                     maze[row,col].westPlanet.name = "West Planet (" + row + ", " + col + ")";
+                    // Change rigid body mass
+                    float newMassW = ((float) sizeW)/2;
+                    maze[row,col].westPlanet.GetComponent<Rigidbody>().mass = newMassW;
                 }
 
                 // Instantiate East planet
                 
-                // Get a random prefab
-                GameObject planetE = _planets[Random.Range(0,_planets.Length)];
-                // Set random size given a range
                 int sizeE = Random.Range(_planetSizeMinimum, _planetSizeMaximum+1);
-                // Instantiate using Photon
-                maze[row,col].eastPlanet = Instantiate(planetE, this.transform.position+new Vector3 (row * _spacing, 0,col*_spacing + _spacing/2f), Quaternion.identity);
-                // Set random size
+                maze[row,col].eastPlanet = PhotonNetwork.Instantiate("BasicPlanet", this.transform.position+new Vector3 (row * _spacing, 0,col*_spacing + _spacing/2f), Quaternion.identity);
                 maze[row,col].eastPlanet.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f) * sizeE;
-                // Set inside GameObject where maze is initiated for cleaner scene hierarchy
                 maze[row,col].eastPlanet.transform.parent = this.transform;
-                // Change name of object with relative coordinate attached;
                 maze[row,col].eastPlanet.name = "East Planet (" + row + ", " + col + ")";
+                float newMassE = ((float) sizeE)/2;
+                maze[row,col].eastPlanet.GetComponent<Rigidbody>().mass = newMassE;
 
                 // Instantiate North planet
                 if (row == 0)
                 {
-                    // Get a random prefab
-                    GameObject planetN = _planets[Random.Range(0,_planets.Length)];
-                    // Set random size given a range
                     int sizeN = Random.Range(_planetSizeMinimum, _planetSizeMaximum+1);
-                    // Instantiate using Photon
-                    maze[row,col].northPlanet = Instantiate(planetN, this.transform.position+new Vector3 (row * _spacing - _spacing/2f, 0, col*_spacing), Quaternion.identity);
-                    // Set random size
+                    maze[row,col].northPlanet = PhotonNetwork.Instantiate("BasicPlanet", this.transform.position+new Vector3 (row * _spacing - _spacing/2f, 0, col*_spacing), Quaternion.identity);
                     maze[row,col].northPlanet.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f) * sizeN;
-                    // Set inside GameObject where maze is initiated for cleaner scene hierarchy
                     maze[row,col].northPlanet.transform.parent = this.transform;
-                    // Change name of object with relative coordinate attached;
                     maze[row,col].northPlanet.name = "North Planet (" + row + ", " + col + ")";
+                    float newMassN = ((float) sizeN)/2;
+                    maze[row,col].eastPlanet.GetComponent<Rigidbody>().mass = newMassN;
                 }
 
                 // Instantiate South planet
                 
-                // Get a random prefab
-                GameObject planetS = _planets[Random.Range(0,_planets.Length)];
-                // Set random size given a range
                 int sizeS = Random.Range(_planetSizeMinimum, _planetSizeMaximum+1);
-                // Instantiate using Photon
-                maze[row,col].southPlanet = Instantiate(planetS, this.transform.position+new Vector3 (row * _spacing + _spacing/2f, 0, col*_spacing), Quaternion.identity);
-                // Set random size
+                maze[row,col].southPlanet = PhotonNetwork.Instantiate("BasicPlanet", this.transform.position+new Vector3 (row * _spacing + _spacing/2f, 0, col*_spacing), Quaternion.identity);
                 maze[row,col].southPlanet.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f) * sizeS;
-                // Set inside GameObject where maze is initiated for cleaner scene hierarchy
                 maze[row,col].southPlanet.transform.parent = this.transform;
-                // Change name of object with relative coordinate attached;
                 maze[row,col].southPlanet.name = "South Planet (" + row + ", " + col + ")";
-                
+                float newMassS = ((float) sizeS)/2;
+                maze[row,col].eastPlanet.GetComponent<Rigidbody>().mass = newMassS;
             }
         }
     }
