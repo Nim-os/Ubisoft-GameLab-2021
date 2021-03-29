6fab355f73a292f7842cd24ae2df593c0fb733c5
sophearahspsp <sophearahspsp@gmail.com>
2021-03-29 00:09:30 -0700

    pickup for gas

---
+++ Assets/MassPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MassPickup : MonoBehaviour
+{
+    public float amount;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")){
+            PlayerPropulsion propulsionScript = other.gameObject.GetComponent<PlayerPropulsion>();
+            propulsionScript.ChangeMass(amount);
+
+            Destroy(gameObject);
+        }
+    }
+}
