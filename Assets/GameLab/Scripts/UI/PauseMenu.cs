2b99949b63b7bb7de6aba43228d738e77dd7e264
moyang.chen@mail.mcgill.ca <moyang.chen@mail.mcgill.ca>
2021-03-31 19:56:36 -0400

    pause menu in local scene

---
+++ Assets/GameLab/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //use escape button to pause
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if(GameIsPaused){
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+    }
+
+
+
+    public void Resume(){
+        PauseMenuUI.SetActive(false);
+        Time.timeScale=1f;
+        GameIsPaused=false;
+    }
+
+
+    void Pause(){
+        PauseMenuUI.SetActive(true);
+        Time.timeScale=0f;
+        GameIsPaused=true;
+    }
+
+
+    public void backToLobby(){
+        Debug.Log("Back to main menu");
+    }
+}
