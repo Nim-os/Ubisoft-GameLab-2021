267338270bd7887857fd72a27ef639d48a5f939e
AprilDeFeu <aprildefeu@protonmail.com>
2021-04-04 04:50:14 -0400

    Redesigned Level One
    - Removed  script as it created conflicts with  script
    
    - Added meshes to pick up items and keys
    
    Fixed Gas bar offline (need to fix for multiplayer)
    
    Changed colors of basic planet(s)
    
    Attempted to create Ice Planet prefab, failed misserably.
    
    Added UI text to indicate endzone direction as well as remaining keys
    
    Made maze smaller
    
    Fixed endzone not appearing bug as well as scene transition once a player reaches this zone.

--- Assets/GameLab/Scripts/Gravitation/RangeDetector.cs
+++ Assets/GameLab/Scripts/Gravitation/RangeDetector.cs
@@ -18,13 +18,14 @@ public class RangeDetector : MonoBehaviour
     private void OnTriggerEnter(Collider other) {
         var hitObject = other.gameObject;
         // if gravitational object && not a planet
-        if (hitObject.GetComponent<BaseGravitation>() && hitObject.GetComponent<BaseGravitation>().enabled && (hitObject.tag != "Planet")){
+        if (hitObject.GetComponent<BaseGravitation>() && hitObject.GetComponent<BaseGravitation>().enabled && (hitObject.tag != "Planet" || hitObject.tag != "mass")){
             body.AddWithinRange(hitObject.GetComponent<BaseGravitation>());
         }
     }
 
     private void OnTriggerExit(Collider other) {
         var hitObject = other.gameObject;
-        body.RemoveWithinRange(hitObject.GetComponent<BaseGravitation>());
+        if (hitObject.tag != "mass")
+            body.RemoveWithinRange(hitObject.GetComponent<BaseGravitation>());
     }
 }
---
+++ Assets/GameLab/Scripts/Stage/Rocks.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Rocks : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == "Player")
+        {
+            col.gameObject.GetComponent<PlayerPropulsion>().ChangeMass(rb.mass);
+            Destroy(this.gameObject);
+        }
+    }
+}
