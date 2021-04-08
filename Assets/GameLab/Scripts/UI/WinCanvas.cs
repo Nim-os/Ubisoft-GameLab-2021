2632a55fc327b61bb6c9171e137dd79262191dfb
moyang.chen@mail.mcgill.ca <moyang.chen@mail.mcgill.ca>
2021-04-08 00:55:14 -0400

    disabled gas bar ui during win scene

--- Assets/GameLab/Scripts/UI/WinCanvas.cs
+++ Assets/GameLab/Scripts/UI/WinCanvas.cs
@@ -6,13 +6,26 @@ public class WinCanvas : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject canvas;
+    public GameObject gasbarUI;
+    public GameObject gastextUI;
+    public GameObject gasBoarder;
 
     void OnEnable(){
         Time.timeScale=0f;
         PauseMenu.GameIsPaused=true;
+
+        gasbarUI.SetActive(false);
+        gastextUI.SetActive(false);
+        gasBoarder.SetActive(false);
     }
 
     public void BacktoLobby(){
+        Time.timeScale=1f;
+        PauseMenu.GameIsPaused=false;
+
+        gasbarUI.SetActive(true);
+        gastextUI.SetActive(true);
+        gasBoarder.SetActive(true);
         
         ServerManager.instance.KickAll();
     }
@@ -21,6 +34,10 @@ public class WinCanvas : MonoBehaviour
     public void RestartLevel(){
         Time.timeScale=1f;
         PauseMenu.GameIsPaused=false;
+
+        gasbarUI.SetActive(true);
+        gastextUI.SetActive(true);
+        gasBoarder.SetActive(true);
 
         UnityEngine.SceneManagement.SceneManager.LoadScene(3);
         canvas.SetActive(false);
