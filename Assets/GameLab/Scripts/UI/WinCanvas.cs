31c8e16ccae5eb256f09306c7a97209ae460fd78
moyang.chen@mail.mcgill.ca <moyang.chen@mail.mcgill.ca>
2021-04-08 00:39:05 -0400

    Finished win window design

---
+++ Assets/GameLab/Scripts/UI/WinCanvas.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCanvas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject canvas;
+
+    void OnEnable(){
+        Time.timeScale=0f;
+        PauseMenu.GameIsPaused=true;
+    }
+
+    public void BacktoLobby(){
+        
+        ServerManager.instance.KickAll();
+    }
+
+
+    public void RestartLevel(){
+        Time.timeScale=1f;
+        PauseMenu.GameIsPaused=false;
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(3);
+        canvas.SetActive(false);
+    }
+}
