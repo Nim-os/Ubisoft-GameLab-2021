64e03e45d0c2e5cfb6e9370852c23f557f0fd745
sophearahspsp <sophearahspsp@gmail.com>
2021-04-07 21:43:46 -0700

    level stages

--- Assets/GameLab/Scripts/Stage/Keys.cs
+++ Assets/GameLab/Scripts/Stage/Keys.cs
@@ -6,15 +6,19 @@ using Photon.Pun;
 public class Keys : MonoBehaviour
 {
     Rigidbody rb;
+    GameObject stageManager;
 
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        stageManager = GameObject.Find("StageManager");
     }
 
     void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.tag == "Player") 
-        PhotonNetwork.Destroy(this.gameObject);
+        if (col.gameObject.tag == "Player") {
+            PhotonNetwork.Destroy(this.gameObject);
+            stageManager.GetComponent<LevelManager>().NewKey();
+        }    
     }
 }
